8b2fd6691ea4d3ecc43c08545eb5635502444a07
veeraprasidh-tech <veeraprasidh@gmail.com>
2026-02-04 19:49:24 +0530

    Create video_analyzer.py

---
+++ video_analyzer.py
@@ -0,0 +1,9 @@
+def analyze_video(video_path):
+    return {
+        "strengths": ["Good stance"],
+        "mistakes": ["Low elbow"],
+        "tips": ["Practice high elbow shots"],
+        "youtube_videos": [
+            "https://youtube.com/results?search_query=high+elbow+batting+drill"
+        ]
+    }
